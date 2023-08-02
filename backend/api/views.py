e87759e53090dee75e2832282a595d9c5f398c4b
Amin <yashar@MacBook-Pro-of-Amin-Ghasemi.local>
2023-08-03 00:30:17 +0300

    todoapp

---
+++ backend/api/views.py
@@ -0,0 +1,86 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import TaskSerializer
+
+from .models import Task
+# Create your views here.
+
+@api_view(['GET'])
+def apiOverview(request):
+	api_urls = {
+		'List':'/task-list/',
+		'Detail View':'/task-detail/<str:pk>/',
+		'Create':'/task-create/',
+		'Update':'/task-update/<str:pk>/',
+		'Delete':'/task-delete/<str:pk>/',
+		}
+
+	return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+	tasks = Task.objects.all().order_by('-id')
+	serializer = TaskSerializer(tasks, many=True)
+	return Response(serializer.data)
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+	tasks = Task.objects.get(id=pk)
+	serializer = TaskSerializer(tasks, many=False)
+	return Response(serializer.data)
+
+
+@api_view(['POST'])
+def taskCreate(request):
+	serializer = TaskSerializer(data=request.data)
+
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
+
+@api_view(['POST'])
+def taskUpdate(request, pk):
+	task = Task.objects.get(id=pk)
+	serializer = TaskSerializer(instance=task, data=request.data)
+
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def taskDelete(request, pk):
+	task = Task.objects.get(id=pk)
+	task.delete()
+
+	return Response('Item succsesfully delete!')
+
+
+
+from django.contrib.auth import authenticate, login, logout
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        
+        user = authenticate(username=username, password=password)
+        
+        if user:
+            login(request, user)
+            return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+class LogoutView(APIView):
+    def post(self, request):
+        logout(request)
+        return Response({'message': 'Logout successful'}, status=status.HTTP_200_OK)
